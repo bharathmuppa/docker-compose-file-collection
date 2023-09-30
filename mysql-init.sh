dfc5318aae02456f41ba761c66eb26666ff48e2e
MrGoddish <bharathmuppa@MrGoddishs-MacBook-Pro.local>
2023-09-30 18:58:33 +0200

    docker command updated which used to have -p in arguments

--- mysql-init.sh
+++ mysql-init.sh
@@ -2,11 +2,11 @@
 # This is file to start redis with user name and password 
 
 read -p 'Databse Name' container_name
-container_name="${container_name:-test_local}"
+container_name="${container_name:-mysql_local}"
 
 # Default user name is root
 
 read -p 'Password [admin]:' secret
 secret="${secret:-toor}"
 
-docker run --name "$container_name" -e MYSQL_ROOT_PASSWORD="$secret" -d mysql:latest -v $(pwd)/mysql/data:/var/lib/mysql -p 3306:3306
+docker run --name "$container_name" -e MYSQL_ROOT_PASSWORD="$secret"  -v $(pwd)/data/mysql:/var/lib/mysql -p 3306:3306 -d mysql:latest
